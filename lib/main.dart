fdb8efcf5df05ef576efeed38c11ed1334acf96f
Leonardo Andrade <leonardoh.deandrade@gmail.com>
2025-05-10 17:46:58 -0300

    feat: configure MaterialApp with Home and Transfer routes and setup HomeBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:afinz_app/features/transfer/presentation/pages/transfer_screen_placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'core/injection.dart' as di;
+import 'features/app/presentation/bloc/home_bloc.dart';
+import 'features/app/presentation/bloc/home_event.dart';
+import 'features/app/presentation/pages/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await di.init();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,12 +20,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Afinz Challenge',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Afinz App')),
-        body: Center(child: Text('Hello Afinz')),
+    return BlocProvider(
+      create: (_) => di.getIt<HomeBloc>()..add(LoadHomeData()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Afinz Home',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (_) => const HomeScreen(),
+          '/transfer': (_) => const TransferScreenPlaceholder(),
+        },
       ),
     );
   }
