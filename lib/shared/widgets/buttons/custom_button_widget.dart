c791619b76209aec990b99dc54d484cfd2ea7e13
Leonardo Andrade <leonardoh.deandrade@gmail.com>
2025-05-13 17:43:46 -0300

    fix(button): make onTap callback nullable
    - Change onTap from required to optional\n- Update type to VoidCallback?\n- Fix type mismatch in transfer screen

--- lib/shared/widgets/buttons/custom_button_widget.dart
+++ lib/shared/widgets/buttons/custom_button_widget.dart
@@ -5,19 +5,21 @@ import '../text/custom_text_widget.dart';
 class CustomButtonWidget extends StatelessWidget {
   final String title;
   final Color? color;
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
+  final bool isLoading;
 
   const CustomButtonWidget({
     super.key,
     required this.title,
     this.color,
-    required this.onTap,
+    this.onTap,
+    this.isLoading = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap,
+      onTap: isLoading ? null : onTap,
       child: Container(
         alignment: Alignment.center,
         margin: const EdgeInsets.only(left: 24, right: 24, bottom: 32),
@@ -27,10 +29,12 @@ class CustomButtonWidget extends StatelessWidget {
           color: color ?? const Color(0xFF00C5CB),
           borderRadius: BorderRadius.circular(16),
         ),
-        child: CustomTextWidget(
-          text: title,
-          textType: TextType.button,
-        ),
+        child: isLoading
+            ? const CircularProgressIndicator(color: Colors.white)
+            : CustomTextWidget(
+                text: title,
+                textType: TextType.button,
+              ),
       ),
     );
   }
